c01816a5528639adf26657b249c11c44e97806af
Sergey <skauzerlfc@gmail.com>
2026-07-17 13:58:17 +0300

    add config vercel

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.concierge.app',
-  appName: 'concierge',
-  webDir: 'dist'
+  appId: "com.concierge.app",
+  appName: "Concierge",
+  webDir: "dist",
 };
 
 export default config;
