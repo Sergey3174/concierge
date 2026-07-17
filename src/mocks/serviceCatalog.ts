b77900dc38053f27908d40c91126b8a430da6e90
Sergey <skauzerlfc@gmail.com>
2026-07-17 10:24:27 +0300

    add new services

--- src/mocks/serviceCatalog.ts
+++ src/mocks/serviceCatalog.ts
@@ -258,6 +258,367 @@ export const serviceCategories: readonly ServiceCategory[] = [
       "serviceCatalog.categories.12.services.16",
       "serviceCatalog.categories.12.services.17"
     ]
+  },
+  {
+    "id": 13,
+    "titleKey": "serviceCatalog.categories.13.title",
+    "services": [
+      "serviceCatalog.categories.13.services.1",
+      "serviceCatalog.categories.13.services.2",
+      "serviceCatalog.categories.13.services.3",
+      "serviceCatalog.categories.13.services.4",
+      "serviceCatalog.categories.13.services.5",
+      "serviceCatalog.categories.13.services.6",
+      "serviceCatalog.categories.13.services.7",
+      "serviceCatalog.categories.13.services.8",
+      "serviceCatalog.categories.13.services.9",
+      "serviceCatalog.categories.13.services.10",
+      "serviceCatalog.categories.13.services.11",
+      "serviceCatalog.categories.13.services.12",
+      "serviceCatalog.categories.13.services.13"
+    ]
+  },
+  {
+    "id": 14,
+    "titleKey": "serviceCatalog.categories.14.title",
+    "services": [
+      "serviceCatalog.categories.14.services.1",
+      "serviceCatalog.categories.14.services.2",
+      "serviceCatalog.categories.14.services.3",
+      "serviceCatalog.categories.14.services.4",
+      "serviceCatalog.categories.14.services.5",
+      "serviceCatalog.categories.14.services.6",
+      "serviceCatalog.categories.14.services.7",
+      "serviceCatalog.categories.14.services.8",
+      "serviceCatalog.categories.14.services.9",
+      "serviceCatalog.categories.14.services.10",
+      "serviceCatalog.categories.14.services.11",
+      "serviceCatalog.categories.14.services.12",
+      "serviceCatalog.categories.14.services.13",
+      "serviceCatalog.categories.14.services.14",
+      "serviceCatalog.categories.14.services.15",
+      "serviceCatalog.categories.14.services.16",
+      "serviceCatalog.categories.14.services.17",
+      "serviceCatalog.categories.14.services.18",
+      "serviceCatalog.categories.14.services.19"
+    ]
+  },
+  {
+    "id": 15,
+    "titleKey": "serviceCatalog.categories.15.title",
+    "services": [
+      "serviceCatalog.categories.15.services.1",
+      "serviceCatalog.categories.15.services.2",
+      "serviceCatalog.categories.15.services.3",
+      "serviceCatalog.categories.15.services.4",
+      "serviceCatalog.categories.15.services.5",
+      "serviceCatalog.categories.15.services.6",
+      "serviceCatalog.categories.15.services.7",
+      "serviceCatalog.categories.15.services.8",
+      "serviceCatalog.categories.15.services.9",
+      "serviceCatalog.categories.15.services.10",
+      "serviceCatalog.categories.15.services.11",
+      "serviceCatalog.categories.15.services.12"
+    ]
+  },
+  {
+    "id": 16,
+    "titleKey": "serviceCatalog.categories.16.title",
+    "services": [
+      "serviceCatalog.categories.16.services.1",
+      "serviceCatalog.categories.16.services.2",
+      "serviceCatalog.categories.16.services.3",
+      "serviceCatalog.categories.16.services.4",
+      "serviceCatalog.categories.16.services.5",
+      "serviceCatalog.categories.16.services.6",
+      "serviceCatalog.categories.16.services.7",
+      "serviceCatalog.categories.16.services.8",
+      "serviceCatalog.categories.16.services.9",
+      "serviceCatalog.categories.16.services.10",
+      "serviceCatalog.categories.16.services.11",
+      "serviceCatalog.categories.16.services.12",
+      "serviceCatalog.categories.16.services.13",
+      "serviceCatalog.categories.16.services.14",
+      "serviceCatalog.categories.16.services.15"
+    ]
+  },
+  {
+    "id": 17,
+    "titleKey": "serviceCatalog.categories.17.title",
+    "services": [
+      "serviceCatalog.categories.17.services.1",
+      "serviceCatalog.categories.17.services.2",
+      "serviceCatalog.categories.17.services.3",
+      "serviceCatalog.categories.17.services.4",
+      "serviceCatalog.categories.17.services.5",
+      "serviceCatalog.categories.17.services.6",
+      "serviceCatalog.categories.17.services.7",
+      "serviceCatalog.categories.17.services.8",
+      "serviceCatalog.categories.17.services.9",
+      "serviceCatalog.categories.17.services.10",
+      "serviceCatalog.categories.17.services.11",
+      "serviceCatalog.categories.17.services.12",
+      "serviceCatalog.categories.17.services.13"
+    ]
+  },
+  {
+    "id": 18,
+    "titleKey": "serviceCatalog.categories.18.title",
+    "services": [
+      "serviceCatalog.categories.18.services.1",
+      "serviceCatalog.categories.18.services.2",
+      "serviceCatalog.categories.18.services.3",
+      "serviceCatalog.categories.18.services.4",
+      "serviceCatalog.categories.18.services.5",
+      "serviceCatalog.categories.18.services.6",
+      "serviceCatalog.categories.18.services.7",
+      "serviceCatalog.categories.18.services.8",
+      "serviceCatalog.categories.18.services.9",
+      "serviceCatalog.categories.18.services.10",
+      "serviceCatalog.categories.18.services.11",
+      "serviceCatalog.categories.18.services.12",
+      "serviceCatalog.categories.18.services.13",
+      "serviceCatalog.categories.18.services.14",
+      "serviceCatalog.categories.18.services.15",
+      "serviceCatalog.categories.18.services.16"
+    ]
+  },
+  {
+    "id": 19,
+    "titleKey": "serviceCatalog.categories.19.title",
+    "services": [
+      "serviceCatalog.categories.19.services.1",
+      "serviceCatalog.categories.19.services.2",
+      "serviceCatalog.categories.19.services.3",
+      "serviceCatalog.categories.19.services.4",
+      "serviceCatalog.categories.19.services.5",
+      "serviceCatalog.categories.19.services.6",
+      "serviceCatalog.categories.19.services.7",
+      "serviceCatalog.categories.19.services.8",
+      "serviceCatalog.categories.19.services.9",
+      "serviceCatalog.categories.19.services.10",
+      "serviceCatalog.categories.19.services.11",
+      "serviceCatalog.categories.19.services.12"
+    ]
+  },
+  {
+    "id": 20,
+    "titleKey": "serviceCatalog.categories.20.title",
+    "services": [
+      "serviceCatalog.categories.20.services.1",
+      "serviceCatalog.categories.20.services.2",
+      "serviceCatalog.categories.20.services.3",
+      "serviceCatalog.categories.20.services.4",
+      "serviceCatalog.categories.20.services.5",
+      "serviceCatalog.categories.20.services.6",
+      "serviceCatalog.categories.20.services.7",
+      "serviceCatalog.categories.20.services.8",
+      "serviceCatalog.categories.20.services.9",
+      "serviceCatalog.categories.20.services.10",
+      "serviceCatalog.categories.20.services.11",
+      "serviceCatalog.categories.20.services.12",
+      "serviceCatalog.categories.20.services.13",
+      "serviceCatalog.categories.20.services.14",
+      "serviceCatalog.categories.20.services.15"
+    ]
+  },
+  {
+    "id": 21,
+    "titleKey": "serviceCatalog.categories.21.title",
+    "services": [
+      "serviceCatalog.categories.21.services.1",
+      "serviceCatalog.categories.21.services.2",
+      "serviceCatalog.categories.21.services.3",
+      "serviceCatalog.categories.21.services.4",
+      "serviceCatalog.categories.21.services.5",
+      "serviceCatalog.categories.21.services.6",
+      "serviceCatalog.categories.21.services.7",
+      "serviceCatalog.categories.21.services.8",
+      "serviceCatalog.categories.21.services.9",
+      "serviceCatalog.categories.21.services.10",
+      "serviceCatalog.categories.21.services.11"
+    ]
+  },
+  {
+    "id": 22,
+    "titleKey": "serviceCatalog.categories.22.title",
+    "services": [
+      "serviceCatalog.categories.22.services.1",
+      "serviceCatalog.categories.22.services.2",
+      "serviceCatalog.categories.22.services.3",
+      "serviceCatalog.categories.22.services.4",
+      "serviceCatalog.categories.22.services.5",
+      "serviceCatalog.categories.22.services.6",
+      "serviceCatalog.categories.22.services.7",
+      "serviceCatalog.categories.22.services.8",
+      "serviceCatalog.categories.22.services.9",
+      "serviceCatalog.categories.22.services.10",
+      "serviceCatalog.categories.22.services.11",
+      "serviceCatalog.categories.22.services.12",
+      "serviceCatalog.categories.22.services.13",
+      "serviceCatalog.categories.22.services.14",
+      "serviceCatalog.categories.22.services.15"
+    ]
+  },
+  {
+    "id": 23,
+    "titleKey": "serviceCatalog.categories.23.title",
+    "services": [
+      "serviceCatalog.categories.23.services.1",
+      "serviceCatalog.categories.23.services.2",
+      "serviceCatalog.categories.23.services.3",
+      "serviceCatalog.categories.23.services.4",
+      "serviceCatalog.categories.23.services.5",
+      "serviceCatalog.categories.23.services.6",
+      "serviceCatalog.categories.23.services.7",
+      "serviceCatalog.categories.23.services.8",
+      "serviceCatalog.categories.23.services.9",
+      "serviceCatalog.categories.23.services.10",
+      "serviceCatalog.categories.23.services.11",
+      "serviceCatalog.categories.23.services.12",
+      "serviceCatalog.categories.23.services.13",
+      "serviceCatalog.categories.23.services.14",
+      "serviceCatalog.categories.23.services.15",
+      "serviceCatalog.categories.23.services.16",
+      "serviceCatalog.categories.23.services.17",
+      "serviceCatalog.categories.23.services.18",
+      "serviceCatalog.categories.23.services.19",
+      "serviceCatalog.categories.23.services.20",
+      "serviceCatalog.categories.23.services.21",
+      "serviceCatalog.categories.23.services.22"
+    ]
+  },
+  {
+    "id": 24,
+    "titleKey": "serviceCatalog.categories.24.title",
+    "services": [
+      "serviceCatalog.categories.24.services.1",
+      "serviceCatalog.categories.24.services.2",
+      "serviceCatalog.categories.24.services.3",
+      "serviceCatalog.categories.24.services.4",
+      "serviceCatalog.categories.24.services.5",
+      "serviceCatalog.categories.24.services.6",
+      "serviceCatalog.categories.24.services.7",
+      "serviceCatalog.categories.24.services.8",
+      "serviceCatalog.categories.24.services.9",
+      "serviceCatalog.categories.24.services.10",
+      "serviceCatalog.categories.24.services.11",
+      "serviceCatalog.categories.24.services.12",
+      "serviceCatalog.categories.24.services.13",
+      "serviceCatalog.categories.24.services.14",
+      "serviceCatalog.categories.24.services.15",
+      "serviceCatalog.categories.24.services.16"
+    ]
+  },
+  {
+    "id": 25,
+    "titleKey": "serviceCatalog.categories.25.title",
+    "services": [
+      "serviceCatalog.categories.25.services.1",
+      "serviceCatalog.categories.25.services.2",
+      "serviceCatalog.categories.25.services.3",
+      "serviceCatalog.categories.25.services.4",
+      "serviceCatalog.categories.25.services.5",
+      "serviceCatalog.categories.25.services.6",
+      "serviceCatalog.categories.25.services.7",
+      "serviceCatalog.categories.25.services.8",
+      "serviceCatalog.categories.25.services.9",
+      "serviceCatalog.categories.25.services.10",
+      "serviceCatalog.categories.25.services.11",
+      "serviceCatalog.categories.25.services.12",
+      "serviceCatalog.categories.25.services.13"
+    ]
+  },
+  {
+    "id": 26,
+    "titleKey": "serviceCatalog.categories.26.title",
+    "services": [
+      "serviceCatalog.categories.26.services.1",
+      "serviceCatalog.categories.26.services.2",
+      "serviceCatalog.categories.26.services.3",
+      "serviceCatalog.categories.26.services.4",
+      "serviceCatalog.categories.26.services.5",
+      "serviceCatalog.categories.26.services.6",
+      "serviceCatalog.categories.26.services.7",
+      "serviceCatalog.categories.26.services.8",
+      "serviceCatalog.categories.26.services.9",
+      "serviceCatalog.categories.26.services.10"
+    ]
+  },
+  {
+    "id": 27,
+    "titleKey": "serviceCatalog.categories.27.title",
+    "services": [
+      "serviceCatalog.categories.27.services.1",
+      "serviceCatalog.categories.27.services.2",
+      "serviceCatalog.categories.27.services.3",
+      "serviceCatalog.categories.27.services.4",
+      "serviceCatalog.categories.27.services.5",
+      "serviceCatalog.categories.27.services.6",
+      "serviceCatalog.categories.27.services.7",
+      "serviceCatalog.categories.27.services.8",
+      "serviceCatalog.categories.27.services.9",
+      "serviceCatalog.categories.27.services.10",
+      "serviceCatalog.categories.27.services.11",
+      "serviceCatalog.categories.27.services.12",
+      "serviceCatalog.categories.27.services.13",
+      "serviceCatalog.categories.27.services.14",
+      "serviceCatalog.categories.27.services.15"
+    ]
+  },
+  {
+    "id": 28,
+    "titleKey": "serviceCatalog.categories.28.title",
+    "services": [
+      "serviceCatalog.categories.28.services.1",
+      "serviceCatalog.categories.28.services.2",
+      "serviceCatalog.categories.28.services.3",
+      "serviceCatalog.categories.28.services.4",
+      "serviceCatalog.categories.28.services.5",
+      "serviceCatalog.categories.28.services.6",
+      "serviceCatalog.categories.28.services.7",
+      "serviceCatalog.categories.28.services.8",
+      "serviceCatalog.categories.28.services.9",
+      "serviceCatalog.categories.28.services.10",
+      "serviceCatalog.categories.28.services.11"
+    ]
+  },
+  {
+    "id": 29,
+    "titleKey": "serviceCatalog.categories.29.title",
+    "services": [
+      "serviceCatalog.categories.29.services.1",
+      "serviceCatalog.categories.29.services.2",
+      "serviceCatalog.categories.29.services.3",
+      "serviceCatalog.categories.29.services.4",
+      "serviceCatalog.categories.29.services.5",
+      "serviceCatalog.categories.29.services.6",
+      "serviceCatalog.categories.29.services.7",
+      "serviceCatalog.categories.29.services.8",
+      "serviceCatalog.categories.29.services.9",
+      "serviceCatalog.categories.29.services.10",
+      "serviceCatalog.categories.29.services.11",
+      "serviceCatalog.categories.29.services.12",
+      "serviceCatalog.categories.29.services.13"
+    ]
+  },
+  {
+    "id": 30,
+    "titleKey": "serviceCatalog.categories.30.title",
+    "services": [
+      "serviceCatalog.categories.30.services.1",
+      "serviceCatalog.categories.30.services.2",
+      "serviceCatalog.categories.30.services.3",
+      "serviceCatalog.categories.30.services.4",
+      "serviceCatalog.categories.30.services.5",
+      "serviceCatalog.categories.30.services.6",
+      "serviceCatalog.categories.30.services.7",
+      "serviceCatalog.categories.30.services.8",
+      "serviceCatalog.categories.30.services.9",
+      "serviceCatalog.categories.30.services.10",
+      "serviceCatalog.categories.30.services.11",
+      "serviceCatalog.categories.30.services.12"
+    ]
   }
 ] as const;
 
